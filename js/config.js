10f4963e3522381c881cf8546af60d18eb5591e9
Horacio Sierra <perezsierra2499@gmail.com>
2026-02-17 23:04:27 -0500

    Fix API errors: remove DateCreated column refs, add CORS headers, use dynamic base URL

--- js/config.js
+++ js/config.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const urlBase = 'http://poosdproject.space/LAMPAPI'; // CHANGE
-=======
-const urlBase = window.location.protocol + '//poosdproject.space/api'; 
->>>>>>> ca6cb5290bab56dce446005f5bab300e850b462c
+const urlBase = window.location.origin + '/api'; 
 const extension = 'php';
 
 let userId = 0;
